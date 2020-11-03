b8f90ce579ffe68d13ecf721fae91b0019d117e9
permabull <rsundh@protonmail.com>
2020-11-03 17:07:24 +0100

    Funny comment

--- flaskapp/init.py
+++ flaskapp/init.py
@@ -3,6 +3,8 @@ from flask_mysqldb import MySQL
 import yaml
 import hashlib, binascii, os
 
+#Yeah booiiiii
+
 app = Flask(__name__)
 
 db = yaml.load(open('/var/www/html/flaskapp/db.yaml'))
@@ -17,7 +19,7 @@ mysql = MySQL(app)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        
+
         user_details = request.form
         username = user_details['username']
         password = user_details['password']
@@ -35,7 +37,7 @@ def index():
                 cur = mysql.connection.cursor()
                 result_value = cur.execute("SELECT password_hash FROM users where username = " + "'" + username + "'")
                 hash_from_sql = cur.fetchmany(1)
-                
+
                 for row in hash_from_sql:
                     pwdhash = row[0]
 
@@ -43,8 +45,8 @@ def index():
                     return render_template('new_login_succes.html')
                 else:
                     return render_template('new_login_fail.html')
-            
-        
+
+
         password_hash = hash_password(password)
 
         cur = mysql.connection.cursor()
@@ -57,22 +59,20 @@ def index():
 def hash_password(password):
     """Hash a password for storing."""
     salt = hashlib.sha256(os.urandom(60)).hexdigest().encode('ascii')
-    pwdhash = hashlib.pbkdf2_hmac('sha512', password.encode('utf-8'), 
+    pwdhash = hashlib.pbkdf2_hmac('sha512', password.encode('utf-8'),
                                 salt, 100000)
     pwdhash = binascii.hexlify(pwdhash)
-    
+
     return (salt + pwdhash).decode('ascii')
 
 def verify_password(stored_password, provided_password):
     """Verify a stored password against one provided by user"""
     salt = stored_password[:64]
     stored_password = stored_password[64:]
-    pwdhash = hashlib.pbkdf2_hmac('sha512', 
-                                  provided_password.encode('utf-8'), 
-                                  salt.encode('ascii'), 
+    pwdhash = hashlib.pbkdf2_hmac('sha512',
+                                  provided_password.encode('utf-8'),
+                                  salt.encode('ascii'),
                                   100000)
     pwdhash = binascii.hexlify(pwdhash).decode('ascii')
-    
+
     return pwdhash == stored_password
-
-
